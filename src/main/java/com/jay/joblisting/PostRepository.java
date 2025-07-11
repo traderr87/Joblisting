5e01774b577c7023b967e10a51af6c272aedb601
trader87 <ironbone98@gmail.com>
2025-07-11 18:37:51 +0700

    Initial commit

---
+++ src/main/java/com/jay/joblisting/PostRepository.java
@@ -0,0 +1,8 @@
+package com.jay.joblisting;
+
+import com.jay.joblisting.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
---
+++ src/main/java/com/jay/joblisting/controller/PostController.java
@@ -0,0 +1,30 @@
+package com.jay.joblisting.controller;
+
+import com.jay.joblisting.PostRepository;
+import com.jay.joblisting.model.Post;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class PostController {
+
+    @Autowired
+    PostRepository repo;
+
+//    @ApiIgnore
+    @RequestMapping(value = "/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getAllPosts(){
+        return repo.findAll();
+    }
+}
